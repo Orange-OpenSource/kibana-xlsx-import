297cf2fc28a5e0de34fa7c8648320b443db6ca70
Frederic Testaniere <frederic.testaniere@orange.com>
2018-01-02 10:47:20 +0100

    english messages

--- public/app.js
+++ public/app.js
@@ -71,9 +71,9 @@ app.controller('xlxsImport', function ($scope, $route, $interval, $http) {
           if(promises.length === bulk_request.length) {   //On check si toutes les promesses sont dans le tableau
             $scope.showSpinner = false                    //On arrete le spinner
             Promise.all(promises).then(function(){        //On verifie si toutes les promesses sont correctes et on envoi un msg
-              alert("Transfert des données terminé");
+              alert("Data transfer complete");
             }).catch(reason => {
-              alert("une erreur est survenue : " + reason);
+              alert("Something wrong happened : " + reason);
             });
           }
       });
@@ -95,7 +95,7 @@ app.directive('importSheetJs', function() {
           if (typeof FileReader !== "undefined") {
 
             var size = (changeEvent.target.files[0].size)/1000000;
-            var message = "Votre fichier depasse la taille limite, voulez vous continuer ?";
+            var message = "File size is too large, do you still want to send it (error might occurs)?";
 
             //Warning si file.size > maxFileSize (TBD)
             if(size > maxFileSize) {
@@ -105,7 +105,7 @@ app.directive('importSheetJs', function() {
                   $scope.$parent.showSpinner = false;
                   $scope.$parent.$apply();
                 });
-                display_data("L'affichage a été limité aux premiers resultats");
+                display_data("Displayed data only on the first five elements");
               }
               else {
                 //On enleve l'affichage des champs et du spinner si la conversion est annulée
